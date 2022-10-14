6503b56129103a50bc9544ba33ba45f7ad3fc41f
mp3096 <66313428+mp3096@users.noreply.github.com>
2022-10-13 20:02:12 -0500

    Added log ,ln
    Symbols inside chat added so user can use log , ln x^2 sign from there

--- FrontEnd/app.py
+++ FrontEnd/app.py
@@ -28,7 +28,7 @@ def about():
 @app.post('/predict')
 def predict():
     text = request.get_json().get("message")
-    response = get_response(text)
+    #response = get_response(text)
     message = {"answer": response}
     return jsonify(message)
 
